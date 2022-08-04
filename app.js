784fedcb310e410c751610f3f68448f00c943fc5
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-08-03 22:45:13 -0700

    Q6

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 //
 //
 //
-function arrSum(num) {
-  let sum = 0;
-
-  for (let i = 1; i <= num; ++i) {
-    sum += i;
-  }
-
-  return sum;
+function calcTime(seconds) {
+  let timerMinutes = Math.floor(seconds / 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  timerSeconds = (seconds % 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  return timerMinutes + ":" + timerSeconds;
 }
 
-console.log(arrSum(4));
+console.log(calcTime(70));
