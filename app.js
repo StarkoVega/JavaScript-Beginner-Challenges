61ebda8078ff564ee8fdca073886589e8741452f
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-08-03 21:46:59 -0700

    Q14

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //
 //
 function isLoggedInAndSubscribed(log, sub) {
-  return log === "LOGGED_IN" && sub === "SUBSCRIBED";
+  return log === "LOGGED_IN" || sub === "SUBSCRIBED";
 }
 
 console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"));
