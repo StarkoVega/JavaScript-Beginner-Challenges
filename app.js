2a0cde89c01073ffbbbfdd30c793ba8f9c8df4a9
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-08-04 20:26:37 -0700

    Q1

--- app.js
+++ app.js
@@ -4,7 +4,19 @@
 //
 //
 //
-function convertToBoolean(arr) {
-  return arr.map((elem) => !!elem);
+function showRating(num) {
+  let integer = Math.floor(num);
+  let rating = "";
+  for (let i = 0; i < integer; ++i) {
+    rating += "*";
+    if (i !== integer - 1) {
+      rating += " ";
+    }
+  }
+  if (num % 1 !== 0) {
+    rating += " .";
+  }
+  return rating;
 }
-console.log(convertToBoolean(["Banana", "", "Orange", "Apple"]));
+
+console.log(showRating(4));
